7957b0aace3b6e749836c6e1b0fc3b96f7730986
Elmar Hinz <t3elmar@gmail.com>
2021-12-13 19:07:17 +0100

    test: compare --version output to version of package.json

--- tests/integration/commands/version.test.js
+++ tests/integration/commands/version.test.js
@@ -1,22 +1,48 @@
 require('chai').should();
+const fs = require('fs');
+const path = require('path');
 const cp = require('child_process');
 const ProjectFixture = require('../../../lib/testing/projectFixture');
+
+function exists(p) {
+  try {
+    fs.accessSync(p);
+    return true;
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return false;
+    }
+    throw error;
+  }
+}
 
 describe('--version', function x() {
   this.timeout(5000);
   let fixture;
+  let version;
 
-  beforeEach(() => {
+  before(() => {
     fixture = new ProjectFixture();
     fixture.setUp();
+    version = cp.execSync('npx ecmake --version').toString();
   });
 
-  afterEach(() => {
+  after(() => {
     fixture.tearDown();
   });
 
-  it('should display the help for <ecmake --version>', () => {
-    const result = cp.execSync('npx ecmake --version').toString();
-    result.should.match(/^\d+\.\d+\.\d+\n$/);
+  it('should display a version number for <ecmake --version>', () => {
+    version.should.match(/^\d+\.\d+\.\d+\n$/);
+  });
+
+  it('should display the version number of package.json', () => {
+    const packageFile = path.resolve(
+      'node_modules',
+      '@ecmake',
+      'ecmake',
+      'package.json'
+    );
+    const pkg = require(packageFile);
+    version.should.include(pkg.version);
   });
 });
