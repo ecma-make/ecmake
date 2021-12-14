a57e8e291644def33d8b8e17820d45e38fcd64f8
Elmar Hinz <t3elmar@gmail.com>
2021-12-14 23:24:05 +0100

    test: do the --target test

---
+++ tests/integration/commands/target.test.js
@@ -0,0 +1,52 @@
+require('chai').should();
+const cp = require('child_process');
+const ProjectFixture = require('../../../lib/testing/projectFixture');
+
+describe('--target', function x() {
+  this.timeout(5000);
+  let fixture;
+  const worldString = 'Hello world!';
+  const marsString = 'Hello mars, here we go!';
+  const configuration = {
+    default: [worldString, marsString],
+    all: [worldString, marsString],
+    'hello.world': [worldString],
+    'hello.planet': [marsString],
+    hello: [],
+    countdown: [],
+    setup: [],
+  };
+
+  function meetsExpectations(text, order) {
+    if(order.length === 0) return true;
+    let i = 0;
+    text.match(/[^\n]+/g).forEach((line) => {
+      i += line.includes(order[i]) ? 1 : 0;
+    });
+    return i === order.length;
+  }
+
+  before(() => {
+    fixture = new ProjectFixture();
+    fixture.setUp();
+    fixture.initCodeFile();
+  });
+
+  after(() => {
+    fixture.tearDown();
+  });
+
+  Object.entries(configuration).forEach(([target, order]) => {
+    describe(target, () => {
+      ['--target', ''].forEach((option) => {
+        const cmd = `ecmake ${option} ${target}`;
+        describe(cmd, () => {
+          it('should return lines in the expected order', () => {
+            const text = cp.execSync(`npx ${cmd}`).toString();
+            meetsExpectations(text, order).should.be.true;
+          });
+        });
+      });
+    });
+  });
+});
