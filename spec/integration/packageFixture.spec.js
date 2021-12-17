ac1f0ce70e1482887c59f76b838967e888f0e391
Elmar Hinz <t3elmar@gmail.com>
2021-12-18 00:07:51 +0100

    test: write class and tests of package fixture

--- spec/integration/packageFixture.spec.js
+++ spec/integration/packageFixture.spec.js
@@ -1,50 +1,165 @@
 require('chai').should();
-const path = require('path');
-const fs = require('fs');
 const cpr = require('child_process');
 
 const PackageFixture = require('../../lib/testing/packageFixture');
 
-before(() => {
-  cpr.execSync('npm uninstall --global @ecmake/ecmake');
-  cpr.execSync('npm unlink --global @ecmake/ecmake');
-});
-
-after(() => {
-  cpr.execSync('npm uninstall --global @ecmake/ecmake');
-  cpr.execSync('npm link');
-});
-
 describe('PackageFixture', function () {
-  describe('constructor', () => {
-    const packageFixture = new PackageFixture();
-    it('should initially set isUp to false', () => {
+  this.timeout(10000);
+  let packageFixture;
+  function clean() {
+    cpr.spawnSync('npm', ['uninstall', '--global', '@ecmake/ecmake']);
+  }
+
+  before(() => {
+    packageFixture = new PackageFixture();
+  });
+
+  after(() => {
+    clean();
+    packageFixture.link();
+  });
+
+  describe('constructor', function () {
+    it('should set package to @ecmake/ecmake', function () {
+      packageFixture.package.should.equal('@ecmake/ecmake');
+    });
+    it('should initially set isUp to false', function () {
       packageFixture.isUp.should.be.false;
     });
   });
 
-  describe('getGlobalPackageState', function() {
-    this.timeout(10000);
-    it('should return null if package not in global directory', function() {
-      const state = PackageFixture.getGlobalPackageState();
-      (state === null).should.be.true;
+  describe('getGlobalPackageState', function () {
+    it('should return null if package not in global directory', function () {
+      clean();
+      (packageFixture.getGlobalPackageState() == null).should.be.true;
     });
-    it('should return linked if package is globally linked', function() {
-      cpr.execSync('npm link');
-      const state = PackageFixture.getGlobalPackageState();
-      state.should.equal('linked');
-      cpr.execSync('npm unlink --global @ecmake/ecmake');
+    it('should return linked if package is globally linked', function () {
+      clean();
+      packageFixture.link();
+      packageFixture.getGlobalPackageState().should.equal('linked');
     });
-    it('should return installed if package is globally installed', function() {
+    it('should return installed if package is globally installed', function () {
+      this.timeout(20000);
+      clean();
       try {
-        cpr.execSync('npm install --global @ecmake/ecmake', { stdio: 'ignore' });
-        const state = PackageFixture.getGlobalPackageState();
+        packageFixture.install();
+        const state = packageFixture.getGlobalPackageState();
         state.should.equal('installed');
-      } catch(error) {
+      } catch (error) {
         this.skip();
-      } finally {
-        cpr.execSync('npm uninstall --global @ecmake/ecmake');
       }
+    });
+  });
+
+  describe('install', function () {
+    before(function () {
+      clean();
+      try {
+        packageFixture.install();
+      } catch (error) {
+        this.skip();
+      }
+    });
+    it('should install the package globally', function () {
+      packageFixture.getGlobalPackageState().should.equal('installed');
+    });
+  });
+
+  describe('uninstall', function () {
+    before(() => {
+      clean();
+      try {
+        packageFixture.install();
+      } catch (error) {
+        this.skip();
+      }
+      packageFixture.uninstall();
+    });
+    it('should uninstall the package globally', function () {
+      (packageFixture.getGlobalPackageState() === null).should.be.true;
+    });
+  });
+
+  describe('link', function () {
+    before(() => {
+      clean();
+      packageFixture.link();
+    });
+    it('should link the package globally', function () {
+      const state = packageFixture.getGlobalPackageState();
+      state.should.equal('linked');
+    });
+  });
+
+  describe('unlink', function () {
+    before(() => {
+      clean();
+      packageFixture.link();
+      packageFixture.unlink();
+    });
+    it('should unlink the package globally', function () {
+      (packageFixture.getGlobalPackageState() === null).should.be.true;
+    });
+  });
+
+  describe('setUp', function () {
+    describe('with empty global installation', function () {
+      before(() => {
+        clean();
+        packageFixture.setUp();
+      });
+      it('should set isUp to true', function () {
+        packageFixture.isUp.should.be.true;
+      });
+      it('should link the package globally', function () {
+        packageFixture.getGlobalPackageState().should.equal('linked');
+      });
+    });
+    describe('with existing global link', function () {
+      before(() => {
+        clean();
+        packageFixture.link();
+      });
+      it('should throw', function () {
+        try {
+          packageFixture.setUp();
+          throw new Error('must not come here');
+        } catch (error) {
+          error.code.should.equal(
+            'ECMAKE_PACKAGE_FIXTURE_GLOBAL_INSTALLATION_EXISTS',
+          );
+        }
+      });
+    });
+
+    describe('with existing global installation', function () {
+      before(() => {
+        clean();
+        packageFixture.install();
+      });
+      it('should throw', function () {
+        try {
+          packageFixture.setUp();
+          throw new Error('must not come here');
+        } catch (error) {
+          error.code.should.equal(
+            'ECMAKE_PACKAGE_FIXTURE_GLOBAL_INSTALLATION_EXISTS',
+          );
+        }
+      });
+    });
+  });
+
+  describe('tearDown', function () {
+    before(() => {
+      clean();
+      packageFixture.tearDown();
+    });
+    it('should set isUp to false', function () {
+      packageFixture.isUp.should.be.false;
+    });
+    it('should unlink the package globally', function () {
+      (packageFixture.getGlobalPackageState() === null).should.be.true;
     });
   });
 });
