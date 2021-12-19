9d0e16b502feeb1eef722bfb64dda709768176d7
Elmar Hinz <t3elmar@gmail.com>
2021-12-19 20:09:07 +0100

    refactor(common): enable modules to be registered for error classes

--- spec/unit/errorClassFactory.spec.js
+++ spec/unit/errorClassFactory.spec.js
@@ -1,36 +1,71 @@
 /* eslint-disable max-classes-per-file */
 require('chai').should();
+const path = require('path');
 const { ErrorClassFactory } = require('../..');
 
-describe('ErrorFacotry', function () {
-  describe('register', function () {
-    const klass = class MyClass {};
-    const errorName = 'MY_ERROR';
-    before(function () {
-      ErrorClassFactory.register(klass, errorName);
+describe(ErrorClassFactory.name, function () {
+  describe('for class', function () {
+    describe('register', function () {
+      const klass = class MyClass {};
+      const errorName = 'MY_ERROR';
+      before(function () {
+        ErrorClassFactory.register(klass, errorName);
+      });
+      it('should register the error within the given class', function () {
+        klass.MY_ERROR.should.be.ok;
+      });
     });
-    it('should register the error within the given class', function () {
-      klass.MY_ERROR.should.be.ok;
+
+    describe('the registered error constructor', function () {
+      const klass = class MyClass {};
+      const errorName = 'MY_ERROR';
+      const message = 'my message';
+      let error;
+      before(function () {
+        ErrorClassFactory.register(klass, errorName);
+        error = new klass.MY_ERROR(message);
+      });
+      it('should be constructor', function () {
+        (error instanceof klass.MY_ERROR).should.be.true;
+      });
+      it('should create an Error', function () {
+        (error instanceof Error).should.be.true;
+      });
+      it('should create an error with a full name', function () {
+        error.name.should.equal(`${klass.name}.${errorName}`);
+      });
     });
   });
 
-  describe('the registered error constructor', function () {
-    const klass = class MyClass {};
-    const errorName = 'MY_ERROR';
-    const message = 'my message';
-    let error;
-    before(function () {
-      ErrorClassFactory.register(klass, errorName);
-      error = new klass.MY_ERROR(message);
+  describe('for module', function () {
+    describe('register', function () {
+      const errorName = 'MY_ERROR';
+      before(function () {
+        ErrorClassFactory.register(module, errorName);
+      });
+      it('should register the error within the given module', function () {
+        module.MY_ERROR.should.be.ok;
+      });
     });
-    it('should be constructor', function () {
-      (error instanceof klass.MY_ERROR).should.be.true;
-    });
-    it('should create an Error', function () {
-      (error instanceof Error).should.be.true;
-    });
-    it('should create an error with a full name', function () {
-      error.name.should.equal(`${klass.name}.${errorName}`);
+
+    describe('the registered error constructor', function () {
+      const errorName = 'MY_ERROR';
+      const message = 'my message';
+      let error;
+      before(function () {
+        ErrorClassFactory.register(module, errorName);
+        error = new module.MY_ERROR(message);
+      });
+      it('should be constructor', function () {
+        (error instanceof module.MY_ERROR).should.be.true;
+      });
+      it('should create an Error', function () {
+        (error instanceof Error).should.be.true;
+      });
+      it('should create an error with a full name', function () {
+        const parentName = `[module: ${path.basename(module.id)}]`;
+        error.name.should.equal(`${parentName}.${errorName}`);
+      });
     });
   });
 });
