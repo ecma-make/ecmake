1be9b7182763339b4310ce3bf1d26579c2a92c0a
Elmar Hinz <t3elmar@gmail.com>
2021-12-20 22:39:17 +0100

    test: test the tool box

--- spec/unit/walker.spec.js
+++
@@ -1,9 +0,0 @@
-require('chai').should();
-const { Walker } = require('../..'); // eslint-disable-line no-unused-vars
-
-describe('Walker', () => {
-  describe('constructor', () => {
-    it.skip('should create a Walker', () => {
-    });
-  });
-});
